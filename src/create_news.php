2a02945f050a0f977eb92c563bc3bff306bcedc6
masaaki630 <134359087+masaaki630@users.noreply.github.com>
2024-01-15 12:01:46 +0900

    create_news変更

--- src/create_news.php
+++ src/create_news.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css"/>
+    <link rel="stylesheet" href="./css/create_news.css">
     <title>記事投稿</title>
 </head>
 <body>
